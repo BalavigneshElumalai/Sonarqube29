9441cf8a3a79bd8d1385dbebc0b4af38daa52846
balavignesh <balavignesh97.e@gmail.com>
2025-08-29 10:55:21 +0000

    todo testing

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -16,7 +16,7 @@ public class Product {
     private String description;
     private double price;
 
-    // Getters and setters
+    // Getters and setters TODO changes
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
